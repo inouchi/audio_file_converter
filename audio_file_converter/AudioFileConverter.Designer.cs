d580bc29f9f99b60fe678928e5bdf420beb1f750
inouchi <inouchi0603@gmail.com>
2022-01-09 13:24:45 +0900

    Init commit

---
+++ audio_file_converter/AudioFileConverter.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace audio_file_converter
+{
+    partial class AudioFileConverter
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectInputFileButton = new System.Windows.Forms.Button();
+            this.MP3RadioButton = new System.Windows.Forms.RadioButton();
+            this.WAVRadioButton = new System.Windows.Forms.RadioButton();
+            this.AACRadioButton = new System.Windows.Forms.RadioButton();
+            this.DeleteInputFilesCheckBox = new System.Windows.Forms.CheckBox();
+            this.AlwaysOnTopCheckBox = new System.Windows.Forms.CheckBox();
+            this.ExsitButton = new System.Windows.Forms.Button();
+            this.ProgressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // SelectInputFileButton
+            // 
+            this.SelectInputFileButton.Location = new System.Drawing.Point(12, 12);
+            this.SelectInputFileButton.Name = "SelectInputFileButton";
+            this.SelectInputFileButton.Size = new System.Drawing.Size(166, 61);
+            this.SelectInputFileButton.TabIndex = 0;
+            this.SelectInputFileButton.Text = "Select Files in File Dialog\r\nor  Drag and Drop Them";
+            this.SelectInputFileButton.UseVisualStyleBackColor = true;
+            this.SelectInputFileButton.Click += new System.EventHandler(this.SelectInputFileButton_Click);
+            // 
+            // MP3RadioButton
+            // 
+            this.MP3RadioButton.AutoSize = true;
+            this.MP3RadioButton.Checked = true;
+            this.MP3RadioButton.Location = new System.Drawing.Point(184, 13);
+            this.MP3RadioButton.Name = "MP3RadioButton";
+            this.MP3RadioButton.Size = new System.Drawing.Size(103, 16);
+            this.MP3RadioButton.TabIndex = 1;
+            this.MP3RadioButton.TabStop = true;
+            this.MP3RadioButton.Text = "Convert to MP3";
+            this.MP3RadioButton.UseVisualStyleBackColor = true;
+            this.MP3RadioButton.CheckedChanged += new System.EventHandler(this.MP3RadioButton_CheckedChanged);
+            // 
+            // WAVRadioButton
+            // 
+            this.WAVRadioButton.AutoSize = true;
+            this.WAVRadioButton.Location = new System.Drawing.Point(184, 35);
+            this.WAVRadioButton.Name = "WAVRadioButton";
+            this.WAVRadioButton.Size = new System.Drawing.Size(106, 16);
+            this.WAVRadioButton.TabIndex = 2;
+            this.WAVRadioButton.Text = "Convert to WAV";
+            this.WAVRadioButton.UseVisualStyleBackColor = true;
+            this.WAVRadioButton.CheckedChanged += new System.EventHandler(this.WAVRadioButton_CheckedChanged);
+            // 
+            // AACRadioButton
+            // 
+            this.AACRadioButton.AutoSize = true;
+            this.AACRadioButton.Location = new System.Drawing.Point(184, 57);
+            this.AACRadioButton.Name = "AACRadioButton";
+            this.AACRadioButton.Size = new System.Drawing.Size(105, 16);
+            this.AACRadioButton.TabIndex = 3;
+            this.AACRadioButton.Text = "Convert to AAC";
+            this.AACRadioButton.UseVisualStyleBackColor = true;
+            this.AACRadioButton.CheckedChanged += new System.EventHandler(this.AACRadioButton_CheckedChanged);
+            // 
+            // DeleteInputFilesCheckBox
+            // 
+            this.DeleteInputFilesCheckBox.AutoSize = true;
+            this.DeleteInputFilesCheckBox.Location = new System.Drawing.Point(12, 114);
+            this.DeleteInputFilesCheckBox.Name = "DeleteInputFilesCheckBox";
+            this.DeleteInputFilesCheckBox.Size = new System.Drawing.Size(115, 16);
+            this.DeleteInputFilesCheckBox.TabIndex = 4;
+            this.DeleteInputFilesCheckBox.Text = "Delete Input Files";
+            this.DeleteInputFilesCheckBox.UseVisualStyleBackColor = true;
+            this.DeleteInputFilesCheckBox.CheckedChanged += new System.EventHandler(this.DeleteInputFilesCheckBox_CheckedChanged);
+            // 
+            // AlwaysOnTopCheckBox
+            // 
+            this.AlwaysOnTopCheckBox.AutoSize = true;
+            this.AlwaysOnTopCheckBox.Location = new System.Drawing.Point(133, 114);
+            this.AlwaysOnTopCheckBox.Name = "AlwaysOnTopCheckBox";
+            this.AlwaysOnTopCheckBox.Size = new System.Drawing.Size(100, 16);
+            this.AlwaysOnTopCheckBox.TabIndex = 5;
+            this.AlwaysOnTopCheckBox.Text = "Always on Top";
+            this.AlwaysOnTopCheckBox.UseVisualStyleBackColor = true;
+            this.AlwaysOnTopCheckBox.CheckedChanged += new System.EventHandler(this.AlwaysOnTopCheckBox_CheckedChanged);
+            // 
+            // ExsitButton
+            // 
+            this.ExsitButton.Location = new System.Drawing.Point(242, 108);
+            this.ExsitButton.Name = "ExsitButton";
+            this.ExsitButton.Size = new System.Drawing.Size(50, 25);
+            this.ExsitButton.TabIndex = 6;
+            this.ExsitButton.Text = "Exsit";
+            this.ExsitButton.UseVisualStyleBackColor = true;
+            this.ExsitButton.Click += new System.EventHandler(this.ExsitButton_Click);
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Location = new System.Drawing.Point(12, 85);
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(280, 15);
+            this.ProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.ProgressBar.TabIndex = 7;
+            // 
+            // AudioFileConverter
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 141);
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.ExsitButton);
+            this.Controls.Add(this.AlwaysOnTopCheckBox);
+            this.Controls.Add(this.DeleteInputFilesCheckBox);
+            this.Controls.Add(this.AACRadioButton);
+            this.Controls.Add(this.WAVRadioButton);
+            this.Controls.Add(this.MP3RadioButton);
+            this.Controls.Add(this.SelectInputFileButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "AudioFileConverter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AudioFileConverter";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.AudioFileConverter_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.AudioFileConverter_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectInputFileButton;
+        private System.Windows.Forms.RadioButton MP3RadioButton;
+        private System.Windows.Forms.RadioButton WAVRadioButton;
+        private System.Windows.Forms.RadioButton AACRadioButton;
+        private System.Windows.Forms.CheckBox DeleteInputFilesCheckBox;
+        private System.Windows.Forms.CheckBox AlwaysOnTopCheckBox;
+        private System.Windows.Forms.Button ExsitButton;
+        private System.Windows.Forms.ProgressBar ProgressBar;
+    }
+}
+
---
+++ audio_file_converter/AudioFileConverter.cs
@@ -0,0 +1,323 @@
+﻿using NAudio.Wave;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+using System.Collections.Generic;
+
+namespace audio_file_converter
+{
+    public partial class AudioFileConverter : Form
+    {
+        private bool mIsDeleteInputeFiles;
+        private ExportFileNameExtension mSelectedExportExtension;
+
+        private static readonly string[] ALLOW_IMPORT_EXTENSIONS = new string[] { ".mp3", ".wav", ".aac" };
+
+        private static readonly Dictionary<ExportFileNameExtension, string> EXPORT_EXTENSIONS = new Dictionary<ExportFileNameExtension, string>()
+        {
+            { ExportFileNameExtension.MP3, ".mp3" },
+            { ExportFileNameExtension.WAV, ".wav" },
+            { ExportFileNameExtension.AAC, ".aac" },
+        };
+
+
+        enum ExportFileNameExtension
+        {
+            MP3,
+            WAV,
+            AAC
+        }
+
+        public AudioFileConverter()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.TopMost = AlwaysOnTopCheckBox.Checked;
+            mIsDeleteInputeFiles = false;
+            mSelectedExportExtension = ExportFileNameExtension.MP3;
+        }
+
+        private MediaFoundationReader OpenInputFile(string fileName)
+        {
+            MediaFoundationReader reader = null;
+
+            try
+            {
+                reader = new MediaFoundationReader(fileName);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Not a supported input file: {fileName}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Console.Error.WriteLine(e.Message);
+            }
+
+            return reader;
+        }
+
+        private bool ConvertAndExportFile(MediaFoundationReader reader, string inputFileName)
+        {
+            string inputFileExtension = Path.GetExtension(inputFileName).ToLower();
+            string outputFileName = GetPathWithoutExtension(inputFileName);
+
+            // 変換前の拡張子と同じ場合は以降の処理はしない
+            if (inputFileExtension == EXPORT_EXTENSIONS[mSelectedExportExtension])
+            {
+                return false;
+            }
+
+            switch (mSelectedExportExtension)
+            {
+                case ExportFileNameExtension.MP3:
+                    try
+                    {
+                        MediaFoundationEncoder.EncodeToMp3(reader, outputFileName + EXPORT_EXTENSIONS[ExportFileNameExtension.MP3]);
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        MessageBox.Show($"Failed to convert a input file: {inputFileName}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Console.Error.WriteLine(ex.Message);
+                        return false;
+                    }
+                    break;
+                case ExportFileNameExtension.WAV:
+                    try
+                    {
+                        WaveFileWriter.CreateWaveFile(outputFileName + EXPORT_EXTENSIONS[ExportFileNameExtension.WAV], reader);
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        MessageBox.Show($"Failed to convert a input file: {inputFileName}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Console.Error.WriteLine(ex.Message);
+                        return false;
+                    }
+                    break;
+                case ExportFileNameExtension.AAC:
+                    try
+                    {
+                        MediaFoundationEncoder.EncodeToAac(reader, outputFileName + EXPORT_EXTENSIONS[ExportFileNameExtension.AAC]);
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        MessageBox.Show($"Failed to convert a input file: {inputFileName}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Console.Error.WriteLine(ex.Message);
+                        return false;
+                    }
+                    break;
+                default:
+                    break;
+
+            }
+
+            return true;
+        }
+
+        private string GetPathWithoutExtension(string path)
+        {
+            string extension = Path.GetExtension(path);
+            return path.Replace(extension, string.Empty);
+        }
+
+        private void StartProgressBar()
+        {
+            ProgressBar.Style = ProgressBarStyle.Marquee;
+        }
+
+        private void StopProgressBar()
+        {
+            ProgressBar.Style = ProgressBarStyle.Continuous;
+        }
+
+        private async void SelectInputFileButton_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+
+            string filter = "Autdio files|";
+            foreach (string extension in ALLOW_IMPORT_EXTENSIONS)
+            {
+                filter = filter + "*" + extension + ";";
+            }
+            ofd.Filter = filter;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.Enabled = false;
+                StartProgressBar();
+
+                await Task.Run(() =>
+                {
+                    foreach (string inputFileName in ofd.FileNames)
+                    {
+                        // 変換対象のファイル読み込む
+                        MediaFoundationReader reader = OpenInputFile(inputFileName);
+
+                        if (reader != null)
+                        {
+                            // 指定した拡張子に変換後、変換対象と同じ場所に出力
+                            bool res = ConvertAndExportFile(reader, inputFileName);
+
+                            if (res && mIsDeleteInputeFiles)
+                            {
+                                // ファイルをゴミ箱へ移動
+                                FileSystem.DeleteFile(inputFileName, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+                            }
+
+                        }
+                    }
+                });
+
+                StopProgressBar();
+                this.Enabled = true;
+            }
+        }
+
+        private void DeleteInputFilesCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            mIsDeleteInputeFiles = DeleteInputFilesCheckBox.Checked;
+        }
+
+        private void AlwaysOnTopCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = AlwaysOnTopCheckBox.Checked;
+        }
+
+        private void ExsitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MP3RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            mSelectedExportExtension = ExportFileNameExtension.MP3;
+        }
+
+        private void WAVRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            mSelectedExportExtension = ExportFileNameExtension.WAV;
+        }
+
+        private void AACRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            mSelectedExportExtension = ExportFileNameExtension.AAC;
+        }
+
+        private bool checkExtension(string fileName)
+        {
+            bool isValied = false;
+            string extension = Path.GetExtension(fileName).ToLower();
+
+            if (Array.IndexOf(ALLOW_IMPORT_EXTENSIONS, extension) != -1)
+            {
+                isValied = true;
+            }
+
+            return isValied;
+        }
+
+        private async void AudioFileConverter_DragDrop(object sender, DragEventArgs e)
+        {
+            this.Enabled = false;
+            StartProgressBar();
+
+            // ドラッグ＆ドロップしたファイルフォルダのパスを取得
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            // ファイルフォルダかを確認
+            bool isFolder = File.GetAttributes(files[0]).HasFlag(FileAttributes.Directory);
+            if (isFolder)
+            {
+                // ディレクトリ以下にあるすべてのファイルを取得
+                var fsEntries = Directory.EnumerateFileSystemEntries(files[0], "*.*", System.IO.SearchOption.AllDirectories);
+
+                await Task.Run(() =>
+                {
+                    foreach (string entry in fsEntries)
+                    {
+                        if (checkExtension(entry))
+                        {
+                            // 変換対象のファイル読み込む
+                            MediaFoundationReader reader = OpenInputFile(entry);
+
+                            if (reader != null)
+                            {
+                                // 指定した拡張子に変換後、変換対象と同じ場所に出力
+                                bool res = ConvertAndExportFile(reader, entry);
+
+                                if (res && mIsDeleteInputeFiles)
+                                {
+                                    // ファイルをゴミ箱へ移動
+                                    FileSystem.DeleteFile(entry, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+                                }
+
+                            }
+                        }
+                    }
+                });
+            }
+            else // ファイル単体またはファイル複数の場合
+            {
+                this.Enabled = false;
+                StartProgressBar();
+
+                await Task.Run(() =>
+                {
+                    foreach (string file in files)
+                    {
+                        if (checkExtension(file))
+                        {
+                            // 変換対象のファイル読み込む
+                            MediaFoundationReader reader = OpenInputFile(file);
+
+                            if (reader != null)
+                            {
+                                // 指定した拡張子に変換後、変換対象と同じ場所に出力
+                                bool res = ConvertAndExportFile(reader, file);
+
+                                if (res && mIsDeleteInputeFiles)
+                                {
+                                    // ファイルをゴミ箱へ移動
+                                    FileSystem.DeleteFile(file, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+                                }
+                            }
+                        }
+                        else
+                        {
+
+                            MessageBox.Show($"Not a supported input file: {file[0]}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                });
+
+                StopProgressBar();
+                this.Enabled = true;
+            }
+        }
+
+        private void AudioFileConverter_DragEnter(object sender, DragEventArgs e)
+        {
+            // マウスポインター形状変更
+            //
+            //  DragDropEffects
+            //  Copy  :データがドロップ先にコピーされようとしている状態
+            //  Move  :データがドロップ先に移動されようとしている状態
+            //  Scroll:データによってドロップ先でスクロールが開始されようとしている状態、あるいは現在スクロール中である状態
+            //  All   :上の3つを組み合わせたもの
+            //  Link  :データのリンクがドロップ先に作成されようとしている状態
+            //  None  :いかなるデータもドロップ先が受け付けようとしない状態
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+    }
+}
